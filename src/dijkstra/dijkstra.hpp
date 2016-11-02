4d0de1c7796b5f04547e792b5d653a18d675007c
Noah Mendoza <NoahMendoza622@gmail.com>
2016-11-01 21:29:07 -0500

    started on more robust version

--- src/dijkstra/dijkstra.hpp
+++ src/dijkstra/dijkstra.hpp
@@ -2,7 +2,14 @@
 //Squad : Croixboyz
 //Professor Engels, Networks CSE 4344
 //dijkstra.cpp
+#ifndef _DIJKSTRA_HPP
+#define _DIJKSTRA_HPP
 
+#include <string>
+#include "../graph/graph.hpp"
 using namespace std;
 
-int dijkstra(int source, int target, vector<vector<int> adjacencyList);
+int dijkstra(int source, int target, vector<vector<int> > adjList);
+void dijsktraV2(const string &source, const string &destination, Graph &g);
+
+#endif
