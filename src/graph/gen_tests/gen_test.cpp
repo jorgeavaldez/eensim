27382f087ba857d81af6cddc49855c76325610ec
Andrew Ksendzoff <aksendzoff@smu.edu>
2016-11-02 19:56:34 -0500

    Started messing with boost graphs

---
+++ src/graph/gen_tests/gen_test.cpp
@@ -0,0 +1,21 @@
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/erdos_renyi_generator.hpp>
+#include <boost/random/linear_congruential.hpp>
+#include <boost/graph/graphviz.hpp>
+
+#include <iostream>
+
+// typedef boost::adjacency_list<> Graph;
+typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::directedS, boost::no_property, int> Graph;
+typedef boost::erdos_renyi_iterator<boost::minstd_rand, Graph> ERGen;
+
+int main()
+{
+  boost::minstd_rand gen;
+  // Create graph with 100 nodes and edges with probability 0.05
+  Graph g(ERGen(gen, 50, 0.75), ERGen(), 50);
+  // std::cout << g << std::endl;
+  boost::write_graphviz(std::cout, g, boost::default_writer(),
+            boost::make_label_writer(boost::get(boost::edge_bundle,g)));
+  return 0;
+}
