a093a858d6322c6f91abab0e3515b60d4f1e4bf6
Andrew Ksendzoff <aksendzoff@smu.edu>
2016-11-07 14:35:05 -0600

    Made weights random, moved generation into graph class with example driver code

--- src/graph2/driver.cpp
+++ src/graph2/driver.cpp
@@ -1,42 +1,14 @@
-#include <stdio>
 #include "graph.hpp"
-#include "gen/gen.hpp"
 
-using namespace std;
+int main(){
+  Graph foo(10);
+  Graph bar(50);
 
-int main() {
+  foo.genGraph(0.8);
+  bar.genGraph(0.5);
 
-  Graph bar(10);
-  boost::minstd_rand gen;
+  foo.dijkstra(0, 10);
+  bar.dijkstra(0, 50);
 
-  int v = 10;
-  double p = 0.65;
-
-  std::ofstream fuckthis;
-  std::stringstream foo;
-  fuckthis.open("shit.txt");
-
-  boostGraph g(ERGen(gen, v, p), ERGen(), v);
-
-  std::pair<boostGraph::edge_iterator,
-  boostGraph::edge_iterator> es = boost::edges(g);
-
-  std::copy(es.first, es.second,
-    std::ostream_iterator<boostGraph::edge_descriptor>{foo, "\n"});
-
-  std::string line;
-  while(std::getline(foo, line)){
-    std::stringstream linestream(line);
-    std::string data;
-    int val1;
-    int val2;
-    boost::erase_all(line, '(');
-    boost::erase_all(line, ')');
-    std::getline(linestream, data, ',');
-    linestream >> val1 >> val2;
-    print(val1 + " " + val2);
-  }
-
-  fuckthis.close();
   return 0;
 }
--- src/graph2/graph.cpp
+++ src/graph2/graph.cpp
@@ -1,74 +1,95 @@
 #include "graph.hpp"
 
-void printSolution(int[], int);
-using namespace std;
 
-namespace fuck{
-  Graph::Graph(int v) {
-      V = v;
-      adj = new list < Node > [V + 1];
-  }
-
-  void Graph::addEdge(int v1, int v2, int w) {
-      Node n1(v2, w);
-      Node n2(v1, w);
-      adj[v1].push_back(n1);
-      adj[v2].push_back(n2);
-  }
-  void Graph::dijkstra(int s, int d) {
-      int dist[d];
-      bool visited[d];
-
-      for (int i = 0; i < d; i++) {
-          dist[i] = INT_MAX;
-          visited[i] = 0;
-      }
-
-      dist[s] = 0;
-
-      for (int i = 0; i < d; i++) {
-          int u = min_dist(dist, visited, d);
-          visited[u] = true;
-          list < Node > ::iterator ob;
-
-          for (ob = adj[u].begin(); ob != adj[u].end(); ++ob) {
-              int v = ( * ob).v;
-              int w = ( * ob).w;
-
-              if (!visited[v] && dist[u] + w < dist[v]) {
-                  dist[v] = dist[u] + w;
-              }
-          }
-      }
-
-      //Printing the distances
-      cout << "Vertex Distance from Source\n";
-
-      for (int i = 0; i < V; i++)
-          cout << i << "\t\t" << dist[i] << "\n";
-  }
-  int Graph::min_dist(int dist[], bool visited[], int len) {
-      int min = INT_MAX, min_index = 1;
-
-      for (int i = 0; i < len; i++) {
-          if (!visited[i] && dist[i] < min) {
-              min = dist[i];
-              min_index = i;
-          }
-      }
-
-      return min_index;
-  }
+Graph::Graph(int v) {
+    V = v;
+    adj = new std::list < Node > [V + 1];
 }
 
-// int main() {
-//     fuck::Graph g(6);
-//     g.addEdge(1, 2, 1);
-//     g.addEdge(1, 5, 2);
-//     g.addEdge(2, 5, 2);
-//     g.addEdge(2, 3, 2);
-//     g.addEdge(3, 6, 3);
-//     g.addEdge(5, 4, 3);
-//     g.addEdge(3, 4, 4);
-//     g.dijkstra(1, 6);
-// }
+void Graph::addEdge(int v1, int v2, int w) {
+    Node n1(v2, w);
+    Node n2(v1, w);
+    adj[v1].push_back(n1);
+    adj[v2].push_back(n2);
+}
+void Graph::dijkstra(int s, int d) {
+    int dist[d];
+    bool visited[d];
+
+    for (int i = 0; i < d; i++) {
+        dist[i] = INT_MAX;
+        visited[i] = 0;
+    }
+
+    dist[s] = 0;
+
+    for (int i = 0; i < d; i++) {
+        int u = min_dist(dist, visited, d);
+        visited[u] = true;
+        std::list < Node > ::iterator ob;
+
+        for (ob = adj[u].begin(); ob != adj[u].end(); ++ob) {
+            int v = ( * ob).v;
+            int w = ( * ob).w;
+
+            if (!visited[v] && dist[u] + w < dist[v]) {
+                dist[v] = dist[u] + w;
+            }
+        }
+    }
+
+    //Printing the distances
+    std::cout << "Vertex Distance from Source\n";
+
+    for (int i = 0; i < V; i++)
+        std::cout << i << "\t\t" << dist[i] << "\n";
+}
+int Graph::min_dist(int dist[], bool visited[], int len) {
+    int min = INT_MAX, min_index = 1;
+
+    for (int i = 0; i < len; i++) {
+        if (!visited[i] && dist[i] < min) {
+            min = dist[i];
+            min_index = i;
+        }
+    }
+
+    return min_index;
+}
+
+void Graph::genGraph(float p){
+  /*
+   * Full disclosure: this is the most janky abomination to the gods of C++
+   * that I have ever written. It kinda works.
+   * p is the probability that nodes will be connected. Refer to the
+   * Wikipedia page for the Erdős–Rényi model, or back to discrete (loljk).
+   */
+  std::stringstream ss;
+
+  std::mt19937 rng;
+  rng.seed(std::random_device()());
+  std::uniform_int_distribution<std::mt19937::result_type> dist(1,20);
+
+  boost::minstd_rand gen;
+  boostGraph g(ERGen(gen, V, p), ERGen(), V);
+
+  std::pair<boostGraph::edge_iterator,
+    boostGraph::edge_iterator> es = boost::edges(g);
+  std::copy(es.first, es.second,
+    std::ostream_iterator<boostGraph::edge_descriptor>{ss, "\n"});
+
+  std::string line;
+
+  while(std::getline(ss, line)){
+    int vert1, vert2, weight;
+    boost::remove_erase_if(line, boost::is_any_of("()"));
+    boost::tokenizer<> tok(line);
+    boost::tokenizer<>::iterator beg = tok.begin();
+    vert1 = std::atoi((*beg).c_str());
+    ++beg;
+    vert2 = std::atoi((*beg).c_str());
+    weight = dist(rng);
+    this->addEdge(vert1, vert2, weight);
+  }
+
+}
