e2286fccc12565e58f914ffb71b8a0a5ed6636a9
Andrew Ksendzoff <aksendzoff@smu.edu>
2016-11-06 23:48:33 -0600

    Generating random grahps but idk how to get the weights out. todo: generate other random weights :shipit:

--- src/graph2/gen/test.cpp
+++ src/graph2/gen/test.cpp
@@ -3,15 +3,20 @@
 int main(){
   boost::minstd_rand gen;
 
-  int v = 43;
+  int v = 10;
   double p = 0.65;
 
-boostGraph g(ERGen(gen, v, p), ERGen(), v);
+  std::ofstream fuckthis;
+  fuckthis.open("shit.txt");
+
+  boostGraph g(ERGen(gen, v, p), ERGen(), v);
 
   std::pair<boostGraph::edge_iterator,
-      boostGraph::edge_iterator> es = boost::edges(g);
+  boostGraph::edge_iterator> es = boost::edges(g);
 
-    std::copy(es.first, es.second,
-      std::ostream_iterator<boostGraph::edge_descriptor>{std::cout, "\n"});
-  return 0;
+  std::copy(es.first, es.second,
+    std::ostream_iterator<boostGraph::edge_descriptor>{fuckthis, "\n"});
+
+  fuckthis.close();
+    return 0;
 }
