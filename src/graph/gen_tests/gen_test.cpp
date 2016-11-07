4dd973b060f9836015786738391664b8e1cd158d
Andrew Ksendzoff <aksendzoff@smu.edu>
2016-11-06 20:51:28 -0600

    Wrapped stuff and started work on pretty python stuff

--- src/graph/gen_tests/gen_test.cpp
+++ src/graph/gen_tests/gen_test.cpp
@@ -4,6 +4,7 @@
 #include <boost/graph/graphviz.hpp>
 
 #include <iostream>
+#include <fstream>
 
 // typedef boost::adjacency_list<> Graph;
 typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::directedS, boost::no_property, int> Graph;
@@ -12,10 +13,14 @@ typedef boost::erdos_renyi_iterator<boost::minstd_rand, Graph> ERGen;
 int main()
 {
   boost::minstd_rand gen;
+  std::ofstream out;
+  out.open("graph.gv");
   // Create graph with 100 nodes and edges with probability 0.05
   Graph g(ERGen(gen, 50, 0.75), ERGen(), 50);
   // std::cout << g << std::endl;
-  boost::write_graphviz(std::cout, g, boost::default_writer(),
+  boost::write_graphviz(out, g, boost::default_writer(),
             boost::make_label_writer(boost::get(boost::edge_bundle,g)));
+
+  out.close();
   return 0;
 }
