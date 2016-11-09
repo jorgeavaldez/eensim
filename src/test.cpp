521c4206cbcbc4e0d1462dc2cbd1cbc39bf20ddc
Andrew Ksendzoff <aksendzoff@smu.edu>
2016-11-08 23:56:28 -0600

    Generating random undirected network with no edge weights

--- src/test.cpp
+++ src/test.cpp
@@ -1,26 +1,37 @@
 #include "Snap.h"
 
-// #include "testclass.hpp"
-
+#include <random>
 #include <iostream>
-
-// void PrintGStats(const char s[], PUNGraph Graph) {
-//   printf("graph %s, nodes %d, edges %d, empty %s\n",
-//       s, Graph->GetNodes(), Graph->GetEdges(),
-//       Graph->Empty() ? "yes" : "no");
-// }
+#include <sstream>
 
 int main() {
-  int nodes = 100;
-  int edges = 200;
+  int nodes = 10;
+  int edges = 15;
   int ubound = 20;
-  // PUNGraph UNGraph;
-  PUNGraph Graph = TSnap::GenRndGnm<PUNGraph>(nodes, edges, false, TInt::Rnd);
-  // PrintGStats("GenerateRndGnmGraph:UNGraph", UNGraph);
-  for (TUNGraph::TEdgeI EI = Graph->BegEI(); EI < Graph->EndEI(); EI++)
-   printf("edge (%d, %d)\n", EI.GetSrcNId(), EI.GetDstNId());
+
+  PUNGraph Graph = TSnap::GenRndGnm<PUNGraph>(nodes, edges, false);
+  PUndirNet Net = new TUndirNet(nodes, edges);
+
+  for(TUNGraph::TNodeI NI = Graph->BegNI(); NI < Graph->EndNI(); NI++){
+    TInt node =  NI.GetId();
+    Net->AddNode(node);
+  }
+
+  for(TUNGraph::TEdgeI EI = Graph->BegEI(); EI < Graph->EndEI(); EI++){
+    TInt src = EI.GetSrcNId();
+    TInt dst = EI.GetDstNId();
+    Net->AddEdge(src, dst);
+  }
 
 
-  std::cout << "test.cpp main hello world" << std::endl;
+
+  for(TUNGraph::TNodeI NI = Graph->BegNI(); NI < Graph->EndNI(); NI++){
+    printf("%d %d %d\n", NI.GetId(), NI.GetOutDeg(), NI.GetInDeg());
+  }
+
+  for(TUndirNet::TEdgeI EI = Net->BegEI(); EI < Net->EndEI(); EI++)
+    printf("edge (%d, %d)\n", EI.GetSrcNId(), EI.GetDstNId());
+
+  printf("# edges: %d\n", Net->GetEdges());
   return 0;
 }
