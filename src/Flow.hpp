afd90d6338ceb3069297a339efd22aa1ec241478
Noah Mendoza <NoahMendoza622@gmail.com>
2016-11-10 00:18:41 -0600

    flow done?

---
+++ src/Flow.hpp
@@ -0,0 +1,27 @@
+#include "Network.hpp"
+
+public struct Flow{
+   public int flowID;
+   public int releaseTime;
+   public int finalTime;
+   public int waitTime;
+   public int numPackets;
+   public int startNodeID;
+   public int endNodeID;
+   public double slowdown;
+
+   public Flow(int id, int releaseT, int numPacks, int startID, int endID)
+   {
+     //inputs
+     flowID = id;
+     releastTime = releaseT;
+     numPackets = numPacks;
+     startNodeID = startID;
+     endNodeID = endID;
+
+     //initialize everything else to 0 cuz im tired
+     finalTime = 0;
+     waitTime = 0;
+     slowdown = 0.0;
+   }	
+}
