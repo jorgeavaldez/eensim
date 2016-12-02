1db551c4ee809054d9578970bcccd9d50f493dcb
Noah <NoahMendoza622@gmail.com>
2016-12-02 00:26:53 +0000

    pathadaptor 1

--- src/IPathAdaptor.hpp
+++ src/IPathAdaptor.hpp
@@ -8,6 +8,8 @@ class IPathAdaptor{
   public:
     IPathAdaptor();
 
+    //IPathAdaptor(Network* n, Node* startNode, Node* endNode);
+
     virtual Flow getFlow(Network*, int, int) = 0;
 
     virtual ~IPathAdaptor() {};
