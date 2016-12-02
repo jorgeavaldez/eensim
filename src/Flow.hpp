609b11399fef1c10964495f23fac783f93030aae
Noah <NoahMendoza622@gmail.com>
2016-12-02 21:30:17 +0000

    added data structures to flow

--- src/Flow.hpp
+++ src/Flow.hpp
@@ -2,29 +2,42 @@
 #define _FLOW_HPP
 
 struct Flow{
-  int flowID;
-  int releaseTime;
-  int finalTime;
-  int waitTime;
-  int numPackets;
-  int startNodeID;
-  int endNodeID;
-  double slowdown;
+   public:
+      int flowID;
+      int releaseTime;
+      int finalTime;
+      int waitTime;
+      int numPackets;
+      int startNodeID;
+      int endNodeID;
+      double slowdown;
 
-  Flow(int id, int releaseT, int numPacks, int startID, int endID)
-  {
-    //inputs
-    flowID = id;
-    releaseTime = releaseT;
-    numPackets = numPacks;
-    startNodeID = startID;
-    endNodeID = endID;
+      Flow(int id, int releaseT, int numPacks, int startID, int endID, std::vector<int> p)
+      {
+      //inputs
+      flowID = id;
+      releaseTime = releaseT;
+      numPackets = numPacks;
+      startNodeID = startID;
+      endNodeID = endID;
+      this.path = p;
 
-    //initialize everything else to 0 cuz im tired
-    finalTime = 0;
-    waitTime = 0;
-    slowdown = 0.0;
-  }
+      //initialize everything else to 0 cuz im tired
+      finalTime = 0;
+      waitTime = 0;
+      slowdown = 0.0;
+      }
+
+      std::vector<int> getPath(){
+	return this.path;
+      }
+
+      void setPath(std::vector<int> p){
+	this.path = p;
+      }
+
+   private:
+      std::vector<int> path;
 };
 
 #endif //_FLOW_HPP
