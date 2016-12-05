c2a17ff992701ac7b9e5e15aeea6a7b3c0524d7c
Jorge Valdez <valdez.a.jorge@gmail.com>
2016-12-04 23:31:00 -0600

    lets see if this is even gonna build #prayforus

--- Makefile.ex
+++ Makefile.ex
@@ -3,6 +3,6 @@
 
 ## Main application file
 
-MAIN = src/andrew
-DEPH = src/Network.hpp src/DijkstraAdaptor.hpp src/MinHopAdaptor.hpp src/MaxBandAdaptor.hpp
-DEPCPP = src/Network.cpp src/DijkstraAdaptor.cpp src/MinHopAdaptor.cpp src/MaxBandAdaptor.cpp
+MAIN = src/eensim
+DEPH = src/Network.hpp src/cortex.hpp src/DijkstraAdaptor.hpp src/MinHopAdaptor.hpp src/MaxBandAdaptor.hpp
+DEPCPP = src/Network.cpp src/cortex.cpp src/DijkstraAdaptor.cpp src/MinHopAdaptor.cpp src/MaxBandAdaptor.cpp
