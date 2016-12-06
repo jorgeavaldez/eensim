80d694b188b07a188f3a9849f9405ada8bce780c
Andrew Ksendzoff <aksendzoff@smu.edu>
2016-12-05 21:04:27 -0600

    fixed the makefile

--- Makefile.ex
+++ Makefile.ex
@@ -4,5 +4,5 @@
 ## Main application file
 
 MAIN = src/eensim
-DEPH = src/Flow.hpp  src/cortex.hpp  src/MinHopAdaptor.hpp src/Network.hpp
-DEPCPP = src/Network.cpp  src/MinHopAdaptor.cpp src/cortex.cpp
+DEPH = src/Flow.hpp  src/cortex.hpp  src/MinHopAdaptor.hpp src/FlowFactory.hpp src/Network.hpp
+DEPCPP = src/Network.cpp  src/MinHopAdaptor.cpp src/FlowFactory.cpp src/cortex.cpp
